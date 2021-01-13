24c9d059db4d0d56f3a43317059018cd98894ba6
Yutaka.T <tak.yuta.biz@gmail.com>
2021-01-13 23:34:18 +0900

    Fix output GitHub

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,13 +1,14 @@
 class Fizzbuzz
   def calculate number
-    if number % 3 == 0 && number % 5 == 0
+    if number.to_s.include? '7'
+    'GitHub Go'
+    elsif number % 3 == 0 && number % 5 == 0
       'fizzbuzz'
     elsif number % 3 == 0
       'fizz'
     elsif number % 5 == 0
       'buzz'
-    elsif number.to_s.include? '7'
-      'GitHub Go'
+
     else
       number
     end
